3d3838a5162036ebccafc87c8d31e76ee1dfd6b5
vishal.parmar <vishalsparmar@yahoo.com>
2019-01-06 09:18:11 +0000

    test

--- runTest.sh
+++ runTest.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-returnValue=$(curl -silent  http://127.0.0.1:8080 | grep V)
+returnValue=$(curl -silent  http://192.168.50.4:80 | grep V)
 echo "$returnValue"
-expectedValue="!!!!!!!Hello World from VP There on Vagrant Ansible Docker NGINx"
+expectedValue="Hello World VP"
 
 if [ "$returnValue" == "$expectedValue" ];then
   echo “Output is Good“;
